bedac4751bda6e62d18c562423fbcfd29cea224c
Pablo Saldarriaga <psaldar2@eafit.edu.co>
2020-02-17 22:37:49 -0500

    Organiza codigo

--- AgruparAccidentes.py
+++ AgruparAccidentes.py
@@ -6,13 +6,36 @@ Created on Sun Feb 16 11:45:38 2020
 """
 import pandas as pd
 import datetime as dt
-#%%
+import funciones
+#%% Importacion de los datos
+
 ### Leer datos
 datos = pd.read_csv('data/Accidentalidad_georreferenciada_2017.csv')
 
 ### Eliminamos los registros donde no tenemos informacion del barrio al cual
 ### corresponde
-datos = datos.dropna(subset = ['BARRIO']).reset_index(drop = True)
+datos = datos.dropna(subset = ['BARRIO']).reset_index(drop = True).rename(columns ={'X':'Lon',
+                                                                                    'Y':'Lat',
+                                                                                    'DIA_NOMBRE':'Dia_sem',
+                                                                                    'DIA': 'Dia',
+                                                                                    'MES':'Mes'})
+
+#%% Procesamiento de los datos
+
+### Reemplazo para los dias de la semana
+semana_limpios = []
+semana = datos['Dia_sem'].values
+reemplazos = {' ':'',
+              'Á':'A',
+              'É':'E'}
+
+for dia in semana:
+    for reemplazo in reemplazos:
+        dia = str(dia).replace(reemplazo,reemplazos[reemplazo])
+        
+    semana_limpios.append(dia)
+
+datos['Dia_sem'] = semana_limpios
 
 ### Limpieza de los nombres de los barrios
 barrios = datos['BARRIO'].values
@@ -34,39 +57,33 @@ for barrio in barrios:
 
 datos['BARRIO'] =  barrios_limpios
 datos['FECHA'] = datos['FECHA'].apply(lambda x: pd.to_datetime(x).strftime('%Y-%m-%d'))
+datos['Hora'] = datos['HORA'].apply(funciones.extraehora)
+datos['TW'] = datos.apply(lambda x: pd.to_datetime(x['FECHA']) + dt.timedelta(hours = x['Hora']), axis = 1)
 
-### elimino registros con barrios raros
+#%%  elimino registros con barrios raros y obtengo la informacion del
+### centroide de los barrios
+
 barrios_raros = ['9086','6001','AUC1']
 datos = datos[~datos['BARRIO'].isin(barrios_raros)].reset_index(drop = True)
 
 ### Coordenada centroide de accidentes para cada barrio
-coords_centroids = datos[['BARRIO','X','Y']].groupby(['BARRIO']).mean().reset_index().rename(columns ={'X':'Lon',
-                                                                                                       'Y':'Lat'})
+coords_centroids = datos[['BARRIO','Lon','Lat']].groupby(['BARRIO']).mean().reset_index()#.rename(columns ={'X':'Lon',
+                                                                                     #                  'Y':'Lat'})
 
 coords_centroids.to_csv('data/centroides_barrios.csv', index = False, sep = ',')
-### Asignar hora del dia para cada accidente (solo el entero de la hora)
-def extraehora(HORA):
-    if 'PM' in HORA:
-        if '12:' not in HORA:
-            hora = int(HORA[:2]) + 12
-        else:
-            hora = int(HORA[:2])
-    else:
-        if '12:' not in HORA:
-            hora = int(HORA[:2])
-        else:
-            hora = 0
-        
-    return hora
+#%%
 
-datos['hour'] = datos['HORA'].apply(extraehora)
-datos['TW'] = datos.apply(lambda x: pd.to_datetime(x['FECHA']) + dt.timedelta(hours = x['hour']), axis = 1)
+datos = datos.drop_duplicates(subset = ['TW','BARRIO'])
 
+cols_ord = ['TW','BARRIO','Lat','Lon','Dia_sem','Mes','Dia','Hora']
+datos[cols_ord].to_csv('data/proc_accidentes_2017.csv', index = False, sep = ',')
+
+#%%    
 ### Agrupar por dia, hora y por barrio
-agrupados = datos.groupby(['MES','DIA','hour','BARRIO']).agg(['count'])
+agrupados = datos.groupby(['Mes','Dia','Hora','BARRIO']).agg(['count'])
 
 ### Miremos un barrio y hora en particular. Por ejemplo, la aguacatala, a las 5pm
 agrupados1 = datos[datos['BARRIO']=='LaAguacatala']
-agrupados2 = agrupados1[agrupados1['hour']==17].groupby(['MES','DIA']).agg(['count']) 
+agrupados2 = agrupados1[agrupados1['Hora']==17].groupby(['Mes','Dia']).agg(['count']) 
 
 ### Sera posible predecir choques usando las condiciones climaticas? 
